2221f6def9a862699d55d0d1b463adb7c0a0952e
CTACOB <star2203@yandex.ru>
2025-08-10 16:57:19 +0300

    initial pom

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ LABEL authors="CTACOB"
 ADD build/libs/KubernetesTestContainers.jar myapp.jar
 EXPOSE 8080
 
-FROM openjdk:17-jdk-slim
+FROM openjdk:21-jdk-slim
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/ru/netology/servicemoneytransfer/TransferControllerIT.java
+++ src/test/java/ru/netology/servicemoneytransfer/TransferControllerIT.java
@@ -1,6 +1,5 @@
 package ru.netology.servicemoneytransfer;
 
-import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
